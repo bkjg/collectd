5670907cf3dacea3e6e3368a1afc58369c396a6c
Barbara Kaczorowska <bkjg@google.com>
2020-07-23 11:07:20 +0200

    Add distribution header file
    This commit add distsribution header file and functions for
    distribution management.

---
+++ src/daemon/distribution.h
@@ -0,0 +1,29 @@
+#ifndef DISTRIBUTION_H
+#define DISTRIBUTION_H
+
+struct distribution_s;
+typedef struct distribution_s distribution_t;
+
+/* function that create new distribution structure and initialize buckets using linear function */
+distribution_t *distribution_new_linear(size_t num_buckets, double size);
+/* function that create new distribution structure and initialize buckets using exponential function */
+distribution_t *distribution_new_exponential(size_t num_buckets, double initial_size, double factor);
+/* function that create new distribution structure and initialize buckets using custom buckets sizes given by the user */
+distribution_t* distribution_new_custom(size_t num_buckets, double *custom_buckets_sizes);
+
+/* function for updating the buckets */
+void distribution_update(distribution_t *d, double gauge);
+
+/* function  for getting the percentile */
+double distribution_percentile(distribution_t *d, double percent);
+
+/* function that calculates average of gauges */
+double distribution_average(distribution_t *d);
+
+/* function that do the clone of distribution structure */
+distribution_t distribution_clone(distribution_t *d);
+
+/* function that do clean up and free all the memory */
+void distribution_destroy(distribution_t *d);
+
+#endif // DISTRIBUTION_H
