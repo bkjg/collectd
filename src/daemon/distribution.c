27bdc61ff001f3fb2d3e668e2399e48fecba11f4
Barbara Kaczorowska <bkjg@google.com>
2020-07-24 09:56:10 +0200

    Fix bugs
    This commit will fix bugs i.e. compilation errors.

--- src/daemon/distribution.c
+++ src/daemon/distribution.c
@@ -14,10 +14,10 @@ bucket_t *bucket_new_linear(size_t num_buckets, double size) {
   bucket_t *buckets = (bucket_t *) calloc(num_buckets, sizeof(bucket_t));
 
   for(size_t i = 0; i < num_buckets; ++i) {
-    buckets[i]->max_boundary = (i + 1) * size; 
+    buckets[i].max_boundary = (i + 1) * size; 
   }
 
-  buckets[num_buckets - 1] = INFINITY;
+  buckets[num_buckets - 1].max_boundary = INFINITY;
 
   return buckets;
 }
@@ -28,25 +28,25 @@ bucket_t *bucket_new_exponential(size_t num_buckets, double factor) {
   double multiplier = factor;
 
   for (int i = 0; i < num_buckets; ++i) {
-    buckets[i]->max_boundary = multiplier;
+    buckets[i].max_boundary = multiplier;
     multiplier *= factor;
   }
 
-  buckets[num_buckets - 1] = INFINITY;
+  buckets[num_buckets - 1].max_boundary = INFINITY;
 
   return buckets;
 }
 
 bucket_t *bucket_new_custom(size_t num_buckets, double *custom_buckets_sizes) {
-  bucket_t *buckets = (distribution_t *) calloc(num_buckets, sizeof(distribution_t));
+  bucket_t *buckets = (bucket_t *) calloc(num_buckets, sizeof(bucket_t));
   double ptr = 0.0;
 
   for (int i = 0; i < num_buckets - 1; ++i) {
-    ptr += custom_buckets_sizes;
-    buckets[i] = ptr;
+    ptr += custom_buckets_sizes[i];
+    buckets[i].max_boundary = ptr;
   }
 
-  buckets[num_buckets -1] = INFINITY;
+  buckets[num_buckets -1].max_boundary = INFINITY;
 
   return buckets;
 }
@@ -75,7 +75,7 @@ distribution_t *distribution_new_custom(size_t num_buckets, double *custom_bucke
   d->buckets = bucket_new_custom(num_buckets, custom_buckets_sizes);
   d->num_buckets = num_buckets;
 
-  return d
+  return d;
 }
 
 void distribution_update(distribution_t *d, double gauge) {
