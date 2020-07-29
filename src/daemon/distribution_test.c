716d63d58735b6f3e8b7e1ff32054689538db1eb
Barbara Kaczorowska <bkjg@google.com>
2020-07-29 13:14:50 +0000

    Add unit tests for distribution_new_linear function
    This commit will add tests for the distribution_new_linear function
    that will check if the function behaves correctly in the case of
    receiving wrong arguments and if returns right values for valid
    arguments.

--- src/daemon/distribution_test.c
+++ src/daemon/distribution_test.c
@@ -1,26 +1,53 @@
 #include "collectd.h"
 
 #include "distribution.h"
+#include "distribution.c"
 #include "testing.h"
 
 DEF_TEST(distribution_new_linear) {
   struct {
     size_t num_buckets;
     double size;
-    int want_err;
     double *want_get;
   } cases[] = {
       {
           .num_buckets = 0,
           .want_get = NULL,
+      },
+      {
+        .num_buckets = 10,
+        .size = -5,
+        .want_get = NULL,
+      },
+      {
+        .num_buckets = 8,
+        .size = 0,
+        .want_get = NULL
+      },
+      {
+        .num_buckets = 10,
+        .size = 2,
+        .want_get = (double[]){2, 4, 6, 8, 10, 12, 14, 16, 18, INFINITY},
       }
   };
 
-  printf("##Test case: 0\n");
-  distribution_t *d = distribution_new_linear(cases[0].num_buckets, cases[0].size);
-  EXPECT_EQ_PTR(NULL, d);
-  distribution_destroy(d);
-  EXPECT_EQ_PTR(NULL, d);
+  for (size_t i = 0; i < (sizeof(cases) / sizeof(cases[0])); ++i) {
+    printf("## Case %zu:\n", i);
+
+    distribution_t *d = distribution_new_linear(cases[i].num_buckets, cases[i].size);
+
+    if (cases[i].want_get == NULL) {
+      EXPECT_EQ_PTR(cases[i].want_get, d);
+    } else {
+      EXPECT_EQ_UINT64(cases[i].num_buckets, d->num_buckets);
+
+      for (size_t j = 0; j < cases[i].num_buckets; ++j) {
+        EXPECT_EQ_DOUBLE(cases[i].want_get[j], d->buckets[j].max_boundary);
+      }
+    }
+
+    distribution_destroy(d);
+  }
 
   return 0;
 }
