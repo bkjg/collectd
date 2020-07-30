e5ad13bb67f12f8fe9bd1803906517e8983fc3bc
Barbara Kaczorowska <bkjg@google.com>
2020-07-30 09:22:29 +0000

    Start implementing tests for distribution_clone function

--- src/daemon/distribution_test.c
+++ src/daemon/distribution_test.c
@@ -1,11 +1,13 @@
 #include "collectd.h"
 
-#include "distribution.h"
 #include "distribution.c"
+#include "distribution.h"
 #include "testing.h"
 
-/* TODO(bkjg): add checking the size of returned array if is it equal to the proper size */
+/* TODO(bkjg): add checking the size of returned array if is it equal to the
+ * proper size */
 /* TODO(bkjg): add checking errno */
+/* TODO(bkjg): add checking sum_gauges and counters in buckets */
 
 DEF_TEST(distribution_new_linear) {
   struct {
@@ -22,22 +24,18 @@ DEF_TEST(distribution_new_linear) {
           .size = -5,
           .want_get = NULL,
       },
-      {
-          .num_buckets = 8,
-          .size = 0,
-          .want_get = NULL
-      },
+      {.num_buckets = 8, .size = 0, .want_get = NULL},
       {
           .num_buckets = 10,
           .size = 2,
-          .want_get = (double[]) {2, 4, 6, 8, 10, 12, 14, 16, 18, INFINITY},
-      }
-  };
+          .want_get = (double[]){2, 4, 6, 8, 10, 12, 14, 16, 18, INFINITY},
+      }};
 
   for (size_t i = 0; i < (sizeof(cases) / sizeof(cases[0])); ++i) {
     printf("## Case %zu:\n", i);
 
-    distribution_t *d = distribution_new_linear(cases[i].num_buckets, cases[i].size);
+    distribution_t *d =
+        distribution_new_linear(cases[i].num_buckets, cases[i].size);
 
     if (cases[i].want_get == NULL) {
       EXPECT_EQ_PTR(cases[i].want_get, d);
@@ -72,35 +70,21 @@ DEF_TEST(distribution_new_exponential) {
           .initial_size = -52,
           .want_get = NULL,
       },
-      {
-          .num_buckets = 8,
-          .initial_size = 0,
-          .want_get = NULL
-      },
-      {
-          .num_buckets = 12,
-          .initial_size = 2,
-          .factor = 0,
-          .want_get = NULL
-      },
-      {
-          .num_buckets = 33,
-          .initial_size = 7,
-          .factor = -5,
-          .want_get = NULL
-      },
+      {.num_buckets = 8, .initial_size = 0, .want_get = NULL},
+      {.num_buckets = 12, .initial_size = 2, .factor = 0, .want_get = NULL},
+      {.num_buckets = 33, .initial_size = 7, .factor = -5, .want_get = NULL},
       {
           .num_buckets = 6,
           .initial_size = 2,
           .factor = 3,
-          .want_get = (double[]) {6, 12, 24, 48, 96, INFINITY},
-      }
-  };
+          .want_get = (double[]){6, 12, 24, 48, 96, INFINITY},
+      }};
 
   for (size_t i = 0; i < (sizeof(cases) / sizeof(cases[0])); ++i) {
     printf("## Case %zu:\n", i);
 
-    distribution_t *d = distribution_new_exponential(cases[i].num_buckets, cases[i].initial_size, cases[i].factor);
+    distribution_t *d = distribution_new_exponential(
+        cases[i].num_buckets, cases[i].initial_size, cases[i].factor);
 
     if (cases[i].want_get == NULL) {
       EXPECT_EQ_PTR(cases[i].want_get, d);
@@ -126,29 +110,28 @@ DEF_TEST(distribution_new_custom) {
   } cases[] = {
       {
           .num_boundaries = 0,
-          .want_get = (double[]) {INFINITY},
+          .want_get = (double[]){INFINITY},
       },
       {
           .num_boundaries = 5,
-          .given_boundaries = (double[]) {5, 4, 6, 7, 8},
+          .given_boundaries = (double[]){5, 4, 6, 7, 8},
           .want_get = NULL,
       },
-      {
-          .num_boundaries = 4,
-          .given_boundaries = (double[]) {-2, 4, 5, 6},
-          .want_get = NULL
-      },
-      {
-          .num_boundaries = 7,
-          .given_boundaries = (double[]) {1.23, 4.76, 6.324, 8.324, 9.342, 16.4234, 90.4234},
-          .want_get = (double[]) {1.23, 4.76, 6.324, 8.324, 9.342, 16.4234, 90.4234, INFINITY}
-      },
+      {.num_boundaries = 4,
+       .given_boundaries = (double[]){-2, 4, 5, 6},
+       .want_get = NULL},
+      {.num_boundaries = 7,
+       .given_boundaries =
+           (double[]){1.23, 4.76, 6.324, 8.324, 9.342, 16.4234, 90.4234},
+       .want_get = (double[]){1.23, 4.76, 6.324, 8.324, 9.342, 16.4234, 90.4234,
+                              INFINITY}},
   };
 
   for (size_t i = 0; i < (sizeof(cases) / sizeof(cases[0])); ++i) {
     printf("## Case %zu:\n", i);
 
-    distribution_t *d = distribution_new_custom(cases[i].num_boundaries, cases[i].given_boundaries);
+    distribution_t *d = distribution_new_custom(cases[i].num_boundaries,
+                                                cases[i].given_boundaries);
 
     if (cases[i].want_get == NULL) {
       EXPECT_EQ_PTR(cases[i].want_get, d);
@@ -168,6 +151,32 @@ DEF_TEST(distribution_new_custom) {
 }
 
 DEF_TEST(distribution_clone) {
+  struct {
+    distribution_t *input_dist;
+    distribution_t *want_get;
+  } cases[] = {
+      {
+          .input_dist = NULL,
+          .want_get = NULL,
+      },
+  };
+
+  for (size_t i = 0; i < (sizeof(cases) / sizeof(cases[0])); ++i) {
+    printf("## Case %zu:\n", i);
+
+    distribution_t *d = distribution_clone(cases[i].input_dist);
+
+    if (cases[i].want_get == NULL) {
+      EXPECT_EQ_PTR(cases[i].want_get, d);
+    } else {
+      CHECK_NOT_NULL(d);
+      // EXPECT_EQ_UINT64(cases[i].num_boundaries + 1, d->num_buckets);
+
+      // for (size_t j = 0; j < cases[i].num_boundaries + 1; ++j) {
+      //  EXPECT_EQ_DOUBLE(cases[i].want_get[j], d->buckets[j].max_boundary);
+      //}
+    }
+  }
 
   return 0;
 }
